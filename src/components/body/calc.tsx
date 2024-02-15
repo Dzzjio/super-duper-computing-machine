c64ca84533eafc735b138cb508cd15bd9a8a3554
Dzzjio <giojio14@gmail.com>
2024-02-15 19:22:20 +0400

    add theme toggler

--- src/components/body/calc.tsx
+++ src/components/body/calc.tsx
@@ -3,16 +3,20 @@
 import { useEffect, useState } from "react";
 import { StyledCalcBody, StyledDisplay, StyledsingleLine } from "./styled";
 
-const Caclulator = () => {
-
+const Calculator = () => {
     const [expression, setExpression] = useState<string>("");
+    const [isLightTheme, setIsLightTheme] = useState<boolean>(true);
+
+    const toggleTheme = () => {
+        setIsLightTheme(prevTheme => !prevTheme);
+    };
 
     const toggleSign = () => {
         setExpression(prevExpression => {
             if (prevExpression[0] === "-") {
-                return prevExpression.substring(1); // Remove "-"
+                return prevExpression.substring(1);
             } else {
-                return "-" + prevExpression; // Add "-"
+                return "-" + prevExpression;
             }
         });
     };
@@ -26,7 +30,7 @@ const Caclulator = () => {
                 setExpression("");
             } else if (buttonValue === "+/-") {
                 toggleSign();
-            } else {
+            } else if (!event.target.classList.contains('toggle-mode')) {
                 setExpression(prevExpression => prevExpression + buttonValue);
             }
         }
@@ -63,46 +67,48 @@ const Caclulator = () => {
     };
 
     return (
-        <StyledCalcBody>
-
-            <StyledDisplay>{expression}</StyledDisplay>
+        <StyledCalcBody style={{ backgroundColor: isLightTheme ? 'white' : 'black', color: isLightTheme ? 'black' : 'white' }}>
+            <StyledDisplay style={{ color: isLightTheme ? 'black' : 'white' }}>{expression}</StyledDisplay>
 
             <StyledsingleLine>
-                <div style={{backgroundColor: 'gray', color: 'black'}}>C</div>
-                <div style={{backgroundColor: 'gray', color: 'black'}}>+/-</div>
-                <div style={{backgroundColor: 'gray', color: 'black'}}>%</div>
-                <div style={{backgroundColor: 'orange', color: 'white'}}>/</div>
+                <div style={{ backgroundColor: 'gray', color: 'black' }}>C</div>
+                <div style={{ backgroundColor: 'gray', color: 'black' }}>+/-</div>
+                <div style={{ backgroundColor: 'gray', color: 'black' }}>%</div>
+                <div style={{ backgroundColor: 'orange', color: 'white' }}>/</div>
             </StyledsingleLine>
 
             <StyledsingleLine>
                 <div>7</div>
                 <div>8</div>
                 <div>9</div>
-                <div style={{backgroundColor: 'orange', color: 'white'}}>*</div>
+                <div style={{ backgroundColor: 'orange', color: 'white' }}>*</div>
             </StyledsingleLine>
 
             <StyledsingleLine>
                 <div>4</div>
                 <div>5</div>
                 <div>6</div>
-                <div style={{backgroundColor: 'orange', color: 'white'}}>-</div>
+                <div style={{ backgroundColor: 'orange', color: 'white' }}>-</div>
             </StyledsingleLine>
 
             <StyledsingleLine>
                 <div>1</div>
                 <div>2</div>
                 <div>3</div>
-                <div style={{backgroundColor: 'orange', color: 'white'}}>+</div>
+                <div style={{ backgroundColor: 'orange', color: 'white' }}>+</div>
             </StyledsingleLine>
 
             <StyledsingleLine>
-                <div style={{width: '100px'}}>0</div>
+                <div style={{ width: '100px' }}>0</div>
                 <div>.</div>
-                <div style={{backgroundColor: 'orange', color: 'white'}}>=</div>
+                <div style={{ backgroundColor: 'orange', color: 'white' }}>=</div>
             </StyledsingleLine>
 
+            <div onClick={toggleTheme} className="toggle-mode" style={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer' }}>
+                {isLightTheme ? '🌞' : '🌙'}
+            </div>
         </StyledCalcBody>
-    )
-}
+    );
+};
 
-export default Caclulator;
+export default Calculator;
